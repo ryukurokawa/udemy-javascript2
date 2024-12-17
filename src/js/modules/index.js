6bad640b1d609b8fc3900fd33dfa15a897831186
ryukurokawa <setsuna1228@gmail.com>
2024-12-17 18:54:56 +0900

    クリエイトリポジトリ

---
+++ src/js/modules/index.js
@@ -0,0 +1,25 @@
+const nav = document.querySelector('#nav');
+const cover = document.querySelector('#cover');
+const appNames = ['todo', 'typing', 'slide-puzzle', 'memory-card', 'life'];
+
+appNames.forEach((appName) => {
+  const menu = document.createElement('a');
+  menu.classList.add('nav-menu');
+  menu.textContent = appName.toUpperCase();
+  menu.addEventListener('click', () => {
+    cover.classList.remove('active');
+    const appEls = document.querySelectorAll('.app');
+    appEls.forEach((appEl) => {
+      appEl.classList.remove('active');
+    });
+    const appEl = document.getElementById(appName);
+    appEl.classList.add('active');
+    const navMenus = document.querySelectorAll('.nav-menu');
+    navMenus.forEach((navMenu) => {
+      navMenu.classList.remove('active');
+    });
+    e.target.classlist.add('active');
+  });
+
+  nav.appendChild(menu);
+});
